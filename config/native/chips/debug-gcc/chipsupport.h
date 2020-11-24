cf851aeda7fd63e720c491a218971a6886ce018c
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-11-24 17:59:27 +0000

    Add debug version of native gcc test configuration.
    Files added:
    
    	* config/native/chips/debug-gcc/chip.cfg
    	* config/native/chips/debug-gcc/chipsupport.c
    	* config/native/chips/debug-gcc/chipsupport.h

---
+++ config/native/chips/debug-gcc/chipsupport.h
@@ -0,0 +1,16 @@
+/* Chip support for Baseline RISC-V configuration
+
+   Copyright (C) 2019 Clemson University
+
+   Contributor Ola Jeppsson <ola.jeppsson@gmail.com>
+
+   This file is part of Embench.
+
+   SPDX-License-Identifier: GPL-3.0-or-later */
+
+#ifndef CHIPSUPPORT_H
+#define CHIPSUPPORT_H
+
+/* Nothing here.  */
+
+#endif
