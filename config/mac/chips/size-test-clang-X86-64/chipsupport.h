55d335342becc376d7f59a9f7dfbd04e19ddb14c
Roger Shepherd <rog@rcjd.net>
2021-04-04 11:01:09 +0100

    These enable and document size and speed banchmarking on x86 and Arm Macs

---
+++ config/mac/chips/size-test-clang-X86-64/chipsupport.h
@@ -0,0 +1,17 @@
+/* Chip support for Apple Mac configuration
+
+   Copyright (C) 2019 Clemson University
+
+   Contributor Ola Jeppsson <ola.jeppsson@gmail.com>
+   Contributor Roger Shepherd <rog@rcjd.net>
+
+   This file is part of Embench.
+
+   SPDX-License-Identifier: GPL-3.0-or-later */
+
+#ifndef CHIPSUPPORT_H
+#define CHIPSUPPORT_H
+
+/* Nothing here.  */
+
+#endif
