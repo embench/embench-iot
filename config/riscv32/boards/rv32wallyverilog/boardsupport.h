85adb896b8de379da9ce33e31739a565573b71a0
DTowersM <dtowersm@gmail.com>
2022-06-07 19:30:15 +0000

    working wally support

---
+++ config/riscv32/boards/rv32wallyverilog/boardsupport.h
@@ -0,0 +1,10 @@
+/* Copyright (C) 2017 Embecosm Limited and University of Bristol
+
+   Contributor Graham Markall <graham.markall@embecosm.com>
+
+   This file is part of Embench and was formerly part of the Bristol/Embecosm
+   Embedded Benchmark Suite.
+
+   SPDX-License-Identifier: GPL-3.0-or-later */
+
+#define CPU_MHZ 1
